484e7d5e49c7374eb47dd723287b0bd3ed92f776
Ghislian Chigozie <chigozieghislian@gmail.com>
2026-06-11 16:09:05 +0100

    added some files to gitignore

---
+++ chatbot.py
@@ -0,0 +1,54 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from ollama import chat
+
+# Load embedding model
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# Load vector database
+db = Chroma(
+    persist_directory="vector_db",
+    embedding_function=embedding_model
+)
+
+while True:
+    question = input("\nAsk a question (or type exit): ")
+
+    if question.lower() == "exit":
+        break
+
+    # Retrieve relevant chunks
+    docs = db.similarity_search(question, k=3)
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+
+    prompt = f"""
+You are a helpful assistant.
+
+Answer ONLY using the information provided below.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+    response = chat(
+        model="llama3.2:latest",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    print("\nAnswer:")
+    print(response["message"]["content"])
